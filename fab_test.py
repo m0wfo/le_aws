7878e2f069968553658303d78408de03f0c5302c
benoit <benoit@logentries.com>
2013-10-08 10:38:18 +0100

    modifications to use fabric

--- fab_test.py
+++ fab_test.py
@@ -1,4 +1,5 @@
 from fabric.api import *
+from fabric import network
 
 from os.path import expanduser
 from paramiko.config import SSHConfig
@@ -6,19 +7,12 @@ from paramiko.config import SSHConfig
 #env.user = 'benoit'
 #env.key_filename = '~/.ssh/id_rsa_logentries'
 
-env.use_ssh_config = True
-env.ssh_config_path = '~/.ssh/config'
-
-try:
-    config_file = file(expanduser('~/.ssh/config'))
-except IOError:
-    pass
-else:
-    config = SSHConfig()
-    config.parse(config_file)
     # From there config._config is a list of dictiionnaries {'host': [hostnames], 'config': {attributes}}
-    for i in range(0,len(config._config)):
-        print str(i), ' - ' , config._config[i]
+
+def print_host_string():
+    for item in env.host_string:
+        print 'i=%s'%unicode(item)
+
 
 def local_uname():
     local('uname -a')
@@ -26,9 +20,18 @@ def local_uname():
 def remote_uname():
     run('uname -a')
 
-def main():
-    local_uname()
-    remote_uname()
-
 if __name__ == "__main__":
-    main()
+#    env.use_ssh_config = True
+#    env.ssh_config_path = '~/.ssh/config'
+
+    try:
+        config_file = file(expanduser('~/.ssh/config'))
+    except IOError:
+        pass
+    else:
+        config = SSHConfig()
+        config.parse(config_file)
+        for i in range(0,len(config._config)):
+            print str(i), ' - ' , config._config[i]
+            print 'ssh_config=%s'%network.ssh_config(config._config[i]['host'][0])
+        execute(remote_uname,hosts=[config._config[i]['host'][0] for i in range(1,len(config._config))])
